c7dabd26c9865b92fbe227d3e22b8712acdbf548
dextro-m <mike.jonesnz@gmail.com>
2021-08-11 16:20:45 +1200

    minor

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,6 +3,8 @@ import styled from "styled-components"
 import { Button } from "./button"
 import { StaticImage } from "gatsby-plugin-image"
 
+//This component
+
 const Hero = () => {
   return (
     <Container>
